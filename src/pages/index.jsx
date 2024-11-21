b74cc2fe7a8b78b5263672f1ff59f0c34fbf2f94
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2024-11-21 14:15:14 +0700

    third commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import postsData from "../posts.json";
-import Articel from "../assets/components/articel";
+import Articel from "../assets/components/Articel";
 import Search from "../assets/components/Search";
 function Homepage() {
     const [posts, setPosts] = useState(postsData);
